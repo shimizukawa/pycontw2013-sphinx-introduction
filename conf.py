fe61c12b682d38be9ca17af566ca09ee778b8e9e
shimizukawa <shimizukawa@gmail.com>
2013-04-20 17:55:20 +0900

    add en po

--- conf.py
+++ conf.py
@@ -13,12 +13,12 @@ copyright = u'2013, Takayuki SHIMIZUKAWA'
 version = release = '1.0'
 exclude_patterns = ['_build']
 templates_path = ['_templates']
+locale_dirs = ['locale']
 pygments_style = 'sphinx'
 extensions = ['sphinxjp.themecore']
 html_logo = 'sphinx-logo.png'
 html_static_path = ['_static']
 html_use_index = False
-
 html_theme = 's6'
 
 
